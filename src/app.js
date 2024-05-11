29c6116b810f5840e2e13008038522c76c979d87
Raphaël Brogat <raphael.brogat@outlook.com>
2024-05-11 11:03:28 +0200

    Update app.js
    Implement the appropriate logic for MS Teams

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 /**
- * Skip the archive page: https://mixmax.slack.com/archives/CB7SHUDMW/p1559265229005700
- * -> https://mixmax.slack.com/messages/CB7SHUDMW/p1559265229005700
+ * Remove "msLaunch=true" parameter, and add "suppressPrompt=true"
  *
- * This runs at document start so we can execute as quickly as possible, mostly to preempt Slack's
+ * This runs at document start so we can execute as quickly as possible, mostly to preempt Teams's
  * annoying JS that tries to open the link in the desktop app, but also for a fast UX.
  */
-window.location.href = window.location.href.replace('/archives/', '/messages/');
+if (window.location.href.indexOf('&msLaunch=true') !=-1){
+    window.location.href = window.location.href.replace('&msLaunch=true', '&suppressPrompt=true');
+}
+
